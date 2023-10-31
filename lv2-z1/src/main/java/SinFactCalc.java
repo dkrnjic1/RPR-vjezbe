d53f353021985ba59ff0a018e85ff6ab285d3b67
Dzana <dkrnjic1@etf.unsa.ba>
2023-10-31 21:25:32 +0100

    Zadatak

---
+++ lv2-z1/src/main/java/SinFactCalc.java
@@ -0,0 +1,11 @@
+public class SinFactCalc {
+    public static void main(String[] args) {
+        int i, faktorijel = 1;
+        int n = Integer.parseInt(args[0]);
+        for (i = 1; i <= n; i++) {
+            faktorijel = faktorijel * i;
+        }
+        double s = Math.sin(n);
+        System.out.printf("Faktorijel broja %d je %d, a sinus je %f", n, faktorijel, s);
+    }
+}
