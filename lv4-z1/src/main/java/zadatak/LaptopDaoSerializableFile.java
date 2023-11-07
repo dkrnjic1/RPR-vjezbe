ba23dd821bd8500adfc95e647abd633bd5a0631d
Dzana <dkrnjic1@etf.unsa.ba>
2023-11-08 00:15:29 +0100

    zadatak i test

---
+++ lv4-z1/src/main/java/zadatak/LaptopDaoSerializableFile.java
@@ -0,0 +1,56 @@
+package zadatak;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class LaptopDaoSeializableFile implements LaptopDao {
+    private  File file;
+    private  ArrayList<Laptop> laptopi;
+
+    public LaptopDaoSeializableFile(File file) {
+        this.file = file;
+        laptopi = new ArrayList<Laptop>();
+    }
+
+    @Override
+    public void dodajLaptopUFile(Laptop laptop) {
+        vratiPodatkeIzDatoteke();
+
+        laptopi.add(laptop);
+
+        try (ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(file))) {
+            outputStream.writeObject(laptopi);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void dodajLaptopUListu(Laptop laptop) {
+        laptopi.add(laptop);
+    }
+
+    @Override
+    public Laptop getLaptop(String procesor) throws NeodgovarajuciProcesorException {
+        for(Laptop l : laptopi) {
+            if(Objects.equals(l.getProcesor(), procesor)) return l;
+        }
+        throw new NeodgovarajuciProcesorException("Ne postoji laptop sa ovim procesorom");
+    }
+
+    @Override
+    public void napuniListu(ArrayList<Laptop> laptopi) {
+        this.laptopi = laptopi;
+    }
+
+    @Override
+    public void vratiPodatkeIzDatoteke() {
+        try (ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(file))) {
+            laptopi = (ArrayList<Laptop>) inputStream.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            // Ako datoteka ne postoji ili nije ispravna, nastavite s praznom listom
+            laptopi = new ArrayList<>();
+        }
+    }
+}
